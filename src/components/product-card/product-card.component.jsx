2795d5bff6dc695e0eb06d111da0b188b80d0806
Abdul Alkout <Abdul.Alkout@gmail.com>
2023-08-09 14:01:54 -0500

    added a cart items count

--- src/components/product-card/product-card.component.jsx
+++ src/components/product-card/product-card.component.jsx
@@ -1,9 +1,12 @@
+import { useContext } from 'react';
+import { CartContext } from '../../context/cart.context';
 import './product-card.style.scss';
 import Button from '../button/button.component';
 
 const ProductCard = ({product}) => {
-
     const {name, price, imageUrl} = product;
+    const {addItemToCart} = useContext(CartContext);
+    const addProductToCart = () => addItemToCart(product);
 
     return (
         <div className='product-card-container'>
@@ -12,7 +15,7 @@ const ProductCard = ({product}) => {
                 <span className='name'>{name}</span>
                 <span className='price'>{price}</span>
             </div>
-            <Button buttonType='inverted'>Add to card</Button>
+            <Button onClick={addProductToCart } buttonType='inverted'>Add to card</Button>
         </div>
     )
 }
